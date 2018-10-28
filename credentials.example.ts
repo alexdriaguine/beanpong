35326b8950cb614aabea5236d356ca203f4ce3b7
alexdriaguine <alexdriagin12@gmail.com>
2018-10-28 19:47:02 +0100

    initial commit

---
+++ credentials.example.ts
@@ -0,0 +1,7 @@
+
+export const privateKey = `-----BEGIN PRIVATE KEY-----
+<YOUR_PRIVATE_KEY>
+-----END PRIVATE KEY-----
+`  
+export const clientEmail = '<YOUR_SERVICE_ACCOUNT_EMAIL>.iam.gserviceaccount.com'
+export const spreadsheetId = '<YOUR_SPREADSHEET_ID>'
